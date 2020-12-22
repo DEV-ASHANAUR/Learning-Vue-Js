0c5970424a813e220eb5060bd725fd23bf605791
dev-ashanaur <ashanour009@gmail.com>
2020-12-22 20:25:51 +0600

    Create Reuseable Button Component

--- src/components/AddUser.vue
+++ src/components/AddUser.vue
@@ -4,15 +4,20 @@
       <form @submit="storeUser">
         <input type="text" v-model="name" placeholder="Enter Name .." required /><br><br>
         <input type="email" v-model="email" placeholder="Enter Email .." required /><br><br>
-        <!-- <input type="submit" value="Add User" />   -->
-        <button type="submit">Add User</button>
+        <Button>Add User</Button><br>
+        <Button>Registation</Button>
       </form>
   </div>
 </template>
 
 <script>
+import Button from './Button'
 export default {
     name: "AddUser",
+    
+    components:{
+        Button,
+    },
     data() {
         return {
             name: "",
@@ -43,18 +48,5 @@ export default {
         /* border-bottom: 2px solid #8566aa; */
         /* outline: none; */
     }
-    button{
-        min-height: 40px;
-        /* width: 40px; */
-        border-radius: 5%;
-        border-color: transparent;
-        background-color: #30336b;
-        color: #fff;
-        font-size: 20px;
-        border: none;
-        outline: none;
-        padding: 0 15px;
-        margin-top: 30px;
-        box-shadow: 5px 5px 15px -5px rgba(0, 0, 0, 0.3);
-    }
+    
 </style>
---
+++ src/components/Button.vue
@@ -0,0 +1,28 @@
+<template>
+  <button type="submit">
+      <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+    name:"Button",
+}
+</script>
+
+<style>
+    button{
+        min-height: 40px;
+        /* width: 40px; */
+        border-radius: 5%;
+        border-color: transparent;
+        background-color: #30336b;
+        color: #fff;
+        font-size: 20px;
+        border: none;
+        outline: none;
+        padding: 0 15px;
+        margin-top: 30px;
+        box-shadow: 5px 5px 15px -5px rgba(0, 0, 0, 0.3);
+    }
+</style>
